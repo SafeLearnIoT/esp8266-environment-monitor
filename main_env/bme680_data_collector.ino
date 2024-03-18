1b656452d619732062e93e44b559472ac441fd73
Mikołaj Macura <mikolajmacura0510@gmail.com>
2024-03-18 22:46:43 +0100

    add schematics + format

--- main_env/bme680_data_collector.ino
+++ main_env/bme680_data_collector.ino
@@ -1,10 +1,15 @@
-void setup_bme(){
+#include "Arduino.h"
+
+void setup_bme()
+{
   uint8_t result = 1;
-  
+
   Serial.println();
-  while (result != 0) {
+  while (result != 0)
+  {
     result = bme.begin();
-    if (result != 0) {
+    if (result != 0)
+    {
       Serial.print("bme ");
       Serial.print(TYPE);
       Serial.println(" begin failure");
@@ -17,20 +22,25 @@ void setup_bme(){
   bme.supportIAQ();
 }
 
-void read_data() {
+void read_data()
+{
   data.clear();
   static uint8_t calibrated = 0;
 
-  if (calibrated == 0) {
-    if (bme.iaqUpdate() == 0) {
+  if (calibrated == 0)
+  {
+    if (bme.iaqUpdate() == 0)
+    {
       sea_level = bme.readSeaLevel(altitude);
       calibrated = 1;
     }
   }
 
-  if (calibrated) {
+  if (calibrated)
+  {
     uint8_t rslt = bme.iaqUpdate();
-    if (rslt == 0) {
+    if (rslt == 0)
+    {
       data["type"] = TYPE;
       data["timestamp"] = millis();
       data["temperature"] = bme.readTemperature();
@@ -39,10 +49,12 @@ void read_data() {
       data["gas resistance"] = bme.readGasResistance();
       data["altitude(m)"] = bme.readAltitude();
       data["calibrated altitude(m)"] = bme.readCalibratedAltitude(sea_level);
-      if (bme.isIAQReady()) {
+      if (bme.isIAQReady())
+      {
         data["IAQ"] = bme.readIAQ();
       }
-      else{
+      else
+      {
         data["IAQ"] = nullptr;
         Serial.print("[IAQ not ready, please wait about ");
         Serial.print((int)(305000 - millis()) / 1000);
@@ -51,7 +63,7 @@ void read_data() {
       String output = "";
       String topic = "/esp8266/";
       topic += TYPE;
-      serializeJsonPretty(data, output);
+      serializeJson(data, output);
       mqtt_client.publish(topic.c_str(), output.c_str());
       Serial.println();
     }
